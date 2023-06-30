a911dcc32e4f391127e7603ed00fa323c8850b76
levispanseriya <69604186+levispanseriya@users.noreply.github.com>
2023-06-30 22:03:31 +0530

    Create App.js
    Hide , Show and Toggle

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import react, { useState } from "react";
+
+function App() {
+  const [status, setStatus] = useState(false);
+  return (
+    <div className="App">
+      {status ? <h1>Hello World!</h1> : null}
+      {/* <button onClick={() => setStatus(false)}>Hidden</button>
+      <button onClick={() => setStatus(true)}>Show</button> */}
+
+      <button onClick={() => setStatus(!status)}>Toggle</button>
+    </div>
+  );
+}
+
+export default App;
