ccc837af42e9ab41eedf8b41239f035be791c33d
levispanseriya <69604186+levispanseriya@users.noreply.github.com>
2023-07-05 09:31:39 +0530

    Update DeleteDataApi

--- DeleteDataApi.js
+++ DeleteDataApi.js
@@ -2,12 +2,29 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const [user, setUser] = useState([]);
+  let users = [
+    { userId: 1, name: "raj", email: "raj@gmail.com", mobile: "9658745632" },
+    {
+      userId: 2,
+      name: "nevil",
+      email: "nevil@gmail.com",
+      mobile: "2563548575",
+    },
+    {
+      userId: 3,
+      name: "parth",
+      email: "parth@gmail.com",
+      mobile: "9856325475",
+    },
+    { userId: 4, name: "mit", email: "mit@gmail.com", mobile: "8569752423" },
+  ];
   useEffect(() => {
-    getList();
+  getList();
   }, []);
 
   function getList() {
-    fetch("localhost:5000/5000/todo/{id}", {
+    fetch("localhost:5000//todo/{id}", {
       method: "DELETE",
     }).then((result) => {
       result.json().then((resp) => {
@@ -15,13 +32,38 @@ const App = () => {
       });
     });
   }
+  console.log(users);
+  function deleteuser(id) {
+    alert(id);
+  }
 
   return (
     <div className="App">
       <h1>DELETE API DATA</h1>
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>EMAIL</td>
+            <td>Mobile</td>
+          </tr>
+          {users.map((item, i) => (
+            <tr key={i}>
+              <td>{item.userId}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              <td>
+                <button onClick={deleteuser(item.id)}>DELETE</button>
+                <button>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default App;
-
